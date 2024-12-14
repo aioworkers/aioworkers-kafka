12349317426293bd02523fec519e543d3fd1dff6
Alexander Malev <malev@somedev.ru>
2024-12-14 23:23:46 +0300

    Improve coverage

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -12,20 +12,25 @@ async def test_get(bootstrap_servers, topic, mocker):
         bootstrap_servers=bootstrap_servers, group_id="test", topics=[topic], content_type=ct
     ) as c:
         m = mocker.patch.object(c, "consumer")
-        msg = m.poll.return_value
+        m.poll.return_value = None
+
+        result = await c.get(timeout=0.1)
+
+        msg = m.poll.return_value = mocker.Mock()
         msg.error.return_value = None
         msg.value.return_value = json.dumps(data).encode()
         msg.headers.return_value = {"content-type": ct}
 
-        result = await c.get(timeout=0.1)
-
         async def produce():
             async with KafkaProducer(content_type=ct) as p:
-                for _ in range(1):
+                for _ in range(2):
                     await asyncio.sleep(0.3)
                     await p.put(data, topic=topic)
 
         task = asyncio.create_task(produce())
+        result = await c.get()
+
+        msg.headers.return_value = {}
         result = await c.get()
 
         await task
