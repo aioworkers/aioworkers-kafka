92241560924cff48f9b3cf1d268b32d2c34d93e6
Alexander Malev <malev@somedev.ru>
2024-12-14 22:25:13 +0300

    add comsumer

---
+++ tests/test_consumer.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from aioworkers_kafka.consumer import KafkaConsumer
+from aioworkers_kafka.producer import KafkaProducer
+
+
+async def test_get(bootstrap_servers):
+    topic = "test"
+    data = {"test": 1}
+    ct = "application/json"
+    async with KafkaConsumer(
+        bootstrap_servers=bootstrap_servers, group_id="test", topics=[topic], content_type=ct
+    ) as c:
+        result = await c.get(timeout=0.1)
+
+        async def produce():
+            async with KafkaProducer(content_type=ct) as p:
+                await asyncio.sleep(1)
+                await p.put(data, topic=topic)
+
+        task = asyncio.create_task(produce())
+        result = await c.get()
+
+        await task
+    assert result == data
