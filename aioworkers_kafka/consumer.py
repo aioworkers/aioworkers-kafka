46c22018a3bd857d2fd31ffa445b163324545464
Alexander Malev <malev@somedev.ru>
2024-12-19 16:43:00 +0300

    Consumer with MappingMessage

--- aioworkers_kafka/consumer.py
+++ aioworkers_kafka/consumer.py
@@ -1,5 +1,6 @@
 import asyncio
-from typing import Dict, List, Optional
+from dataclasses import dataclass
+from typing import Any, Dict, List, Mapping, Optional
 
 import confluent_kafka
 from aioworkers.core.base import AbstractConnector, AbstractReader, ExecutorEntity
@@ -8,6 +9,54 @@ from aioworkers.core.formatter import FormattedEntity
 from confluent_kafka import KafkaError
 
 from aioworkers_kafka.utils import flat_conf
+
+
+@dataclass
+class IncommingMessage(Mapping):
+    value: Any
+    topic: str
+    key: Optional[bytes]
+    headers: Mapping[str, bytes]
+
+    def __getitem__(self, key: str) -> bytes:
+        return self.headers[key]
+
+    def __iter__(self):
+        return iter(())
+
+    def __len__(self) -> int:
+        return 0
+
+
+@dataclass
+class RawMessage(IncommingMessage):
+    value: bytes
+    topic: str
+    key: Optional[bytes]
+    headers: Mapping[str, bytes]
+
+
+@dataclass
+class MappingMessage(IncommingMessage):
+    value: Mapping[str, Any]
+    topic: str
+    key: Optional[bytes]
+    headers: Mapping[str, bytes]
+
+    def __getitem__(self, key: str) -> Any:
+        try:
+            return self.value[key]
+        except KeyError:
+            if v := self.headers.get(key):
+                return v
+            else:
+                raise
+
+    def __iter__(self):
+        return iter(self.value)
+
+    def __len__(self) -> int:
+        return len(self.value)
 
 
 class KafkaConsumer(AbstractReader, FormattedEntity, ExecutorEntity, AbstractConnector):
@@ -51,22 +100,37 @@ class KafkaConsumer(AbstractReader, FormattedEntity, ExecutorEntity, AbstractCon
     async def disconnect(self):
         await self.run_in_executor(self.consumer.close)
 
-    async def get(self, timeout: Optional[float] = None):
+    async def get(self, timeout: Optional[float] = None) -> Optional[IncommingMessage]:
         poll_timeout = timeout or 1.0
         while True:
             msg = await self.run_in_executor(self.consumer.poll, poll_timeout)
             if msg is None:
                 if timeout is not None:
-                    break
+                    return None
             elif msg.error():
                 if msg.error().code() not in {KafkaError._PARTITION_EOF}:
                     self.logger.error("Consume with error %s", msg.error())
                 await asyncio.sleep(1)
-            elif (h := msg.headers()) and (ct := h.get("content-type")):
-                f = self.registry.get(ct)
-                return f.decode(msg.value())
             else:
-                return self.decode(msg.value())
+                return self.decode_msg(msg)
+
+    def decode_msg(self, msg: confluent_kafka.Message) -> IncommingMessage:
+        if h := msg.headers():
+            headers = dict(h)
+        else:
+            headers = {}
+
+        raw_value = msg.value()
+        if ct := headers.get("content-type"):
+            f = self.registry.get(ct)
+            value = f.decode(msg.value())
+        else:
+            value = self.decode(msg.value())
+
+        if type(value) is bytes:
+            return RawMessage(value=raw_value, topic=msg.topic(), key=msg.key(), headers=headers)
+        else:
+            return MappingMessage(value=value, topic=msg.topic(), key=msg.key(), headers=headers)
 
     async def __aenter__(self):
         self.set_context(Context())
